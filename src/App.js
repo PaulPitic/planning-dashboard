230b2baa703c0d234bd4900c9ae9734cb8b51a6c
PaulPitic <nwmxtn7tm7@privaterelay.appleid.com>
2026-04-25 16:01:53 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -313,55 +313,63 @@ export default function App() {
 
       setBoardData(data.board || createBoard());
 
-      /* AUTOMATIC STAFF MERGE FIX */
       const savedStaff = data.staff || {};
 
       setStaff({
-  supervisors: [
-    ...new Set([
-      ...(savedStaff.supervisors || []),
-      ...defaultStaff.supervisors,
-    ]),
-  ],
+        supervisors: [
+          ...new Set([
+            ...(savedStaff.supervisors || []),
+            ...defaultStaff.supervisors,
+          ]),
+        ],
 
-  coordinators: [
-    ...new Set([
-      ...(savedStaff.coordinators || []),
-      ...defaultStaff.coordinators,
-    ]),
-  ],
+        coordinators: [
+          ...new Set([
+            ...(savedStaff.coordinators || []),
+            ...defaultStaff.coordinators,
+          ]),
+        ],
 
-  "Team A": [
-    ...new Set([
-      ...(savedStaff["Team A"] || []),
-      ...defaultStaff["Team A"],
-    ]),
-  ],
+        "Team A": [
+          ...new Set([
+            ...(savedStaff["Team A"] || []),
+            ...defaultStaff["Team A"],
+          ]),
+        ],
 
-  "Team B": [
-    ...new Set([
-      ...(savedStaff["Team B"] || []),
-      ...defaultStaff["Team B"],
-    ]),
-  ],
+        "Team B": [
+          ...new Set([
+            ...(savedStaff["Team B"] || []),
+            ...defaultStaff["Team B"],
+          ]),
+        ],
 
-  "A-FLOW A": [
-    ...new Set([
-      ...(savedStaff["A-FLOW A"] || []),
-      ...defaultStaff["A-FLOW A"],
-    ]),
-  ],
+        "A-FLOW A": [
+          ...new Set([
+            ...(savedStaff["A-FLOW A"] || []),
+            ...defaultStaff["A-FLOW A"],
+          ]),
+        ],
 
-  "A-FLOW B": [
-    ...new Set([
-      ...(savedStaff["A-FLOW B"] || []),
-      ...defaultStaff["A-FLOW B"],
-    ]),
-  ],
-});
+        "A-FLOW B": [
+          ...new Set([
+            ...(savedStaff["A-FLOW B"] || []),
+            ...defaultStaff["A-FLOW B"],
+          ]),
+        ],
+      });
 
-setLocked(data.locked ?? true);
-setTeam(data.currentTeam || "Team A");
+      setLocked(data.locked ?? true);
+      setTeam(data.currentTeam || "Team A");
+    } else {
+      await setDoc(ref, {
+        board: createBoard(),
+        staff: defaultStaff,
+        locked: true,
+        currentTeam: "Team A",
+      });
+    }
+  });
 
   return () => unsub();
 }, []);
